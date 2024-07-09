f64c7211302d5a077627d7554d26ff09e7858efa
davmantor <98769095+davmantor@users.noreply.github.com>
2024-07-09 14:18:41 -0700

    Reapply "Merge branch 'main' of https://github.com/davmantor/FigJam-Widgets"
    This reverts commit 81ea39e124caff4c055106e5e380a2bc63ada285.

--- onboarding/SIP 2024/Bonniefolder/widget/widget-src/code.tsx
+++ onboarding/SIP 2024/Bonniefolder/widget/widget-src/code.tsx
@@ -1,48 +1,82 @@
 const { widget } = figma 
-const { useSyncedState,  Text, SVG, Rectangle, Frame} = widget 
+const { useSyncedState, usePropertyMenu,Text, SVG, Rectangle, Frame,AutoLayout,Input} = widget 
 
-const SampleData = [10, 50, 23, 48, 38, 14]; 
+
 const colors = ["8dd3c7", "ffffb3", "bebada", "fb8072", "80b1d3", "fdb462"]; 
-const labels = ["Bar 1", "Bar 2", "Bar 3", "Bar 4", "Bar 5", "Bar 6"]; 
-const X_BAR = 50
-const Y_BAR = 100
+const labels = ["input 1", "input 2", "input 3", "input 4", "input 5", "input 6"]; 
+const sampleData=[10,20,30,40,50,60]
 
 const ADD_BUTTON_X = 150;
 const ADD_BUTTON_Y = 20;
 
-function BarGraphWidget() {
-  const [data] = useSyncedState("data", SampleData); 
+function BarGraphWidget() 
+const [sampleData, setSampleData] = useSyncedState
   const frameWidth = 400;
   const frameHeight = 200;
   const barWidth = 40;
   const barSpacing = 60;
   const barBaseY = frameHeight - 30; 
   const labelY = barBaseY + 10; 
-  const title = "barchart"
-  const [count, setCount] = useSyncedState('count', 0)
- 
- 
+  const title = "chart"
+  const incremenValue=(index:number)=>{
+    const newData=[...SampleData];
+    newData[index]+=10;
 
+}
+ 
   return (
-    <Frame width={frameWidth} height={frameHeight}>
+  <AutoLayout
+  directionm="horizontal" x={10} y={frameHeight +10}> 
+ data.map(_, index) = useSyncedState
+          <svg key={index} width="24" height="24" onClick={() => incrementValue(index)}>
+            {/* SVG for increment button */}
+            <rect width="24" height="24" fill="lightgray" />
+            <text x="12" y="16" textAnchor="middle" fill="black">+</text>
+          </svg>
+         
+        
+  <Frame
+width={frameWidth} height={frameHeight}>
+  {data.map((height, index))}
+  {title} 
+  </Frame>
+
+  ╵
+
+  
+  <Rectangle
+            key={`bar-${index}`} 
+            x={barSpacing * index + 20} 
+            y={barBaseY - value} 
+            width={50}
+            height={60}
+            fill={`#FFFFFF`} 
+            rotation={180}
+          />
+       
+         
       <Text
         x={frameWidth / 2 - 110} 
         y={20} 
         fontSize={16}
         fontWeight="bold"
       >
+     
+        
+        
         {title} 
       </Text>
-      {data.map((value, index) => ( 
-        <>
+    
+     
+   
+
+
 
      
       
     
       
-      <Text fontSize={32} width={42} horizontalAlignText={'center'}>
-        {count}
-      </Text>
+      
       <SVG
         src={`<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect width="30" height="30" rx="15" fill="white"/>
@@ -50,38 +84,20 @@ function BarGraphWidget() {
         <rect x="0.5" y="0.5" width="29" height="29" rx="14.5" stroke="black" stroke-opacity="0.1"/>
         </svg>`}
         onClick={() => {
-          setCount(count + 1)
+        setCount(count + 1)
         }}
       ></SVG>
    
-  
+   </AutoLayout>)
     
-          <Rectangle
-            key={`bar-${index}`} 
-            x={barSpacing * index + 20} 
-            y={barBaseY - value} 
-            width={barWidth}
-            height={value}
-            fill={`#${colors[index]}`} 
-            rotation={180}
-          />
-          <Text
-            key={`label-${index}`} 
-            x={barSpacing * index + 25} 
-            y={labelY}
-            fontSize={12}
-          >
-            {labels[index]}
-          </Text>
-        </>
-      ))}
-    </Frame>
-  );
-}
-
-widget.register(BarGraphWidget)
+        
+        
+      (labels[index])
+        
+        
+    
+  
+  
 
 
-
-
-
+widget.register (BarGraphWidget)
