3ba7f7d416a8d9292860e338b180699b6d0d25fc
zhilinw08 <zhilinwang08@gmail.com>
2024-07-12 15:33:44 -0700

    Updated Bar Graph
    With nested Auto-Layouts and corrected placement of the numbers above the graphs. I will work on implementing a subtraction feature next.

--- onboarding/SIP 2024/ZiaFolder/firstwidget/widget-src/code.tsx
+++ onboarding/SIP 2024/ZiaFolder/firstwidget/widget-src/code.tsx
@@ -3,7 +3,6 @@ const { widget } = figma
 
 const { useSyncedState, usePropertyMenu, AutoLayout, Text, SVG, Rectangle, Frame, Input } = widget
 const colors = ['FF0000', '00FF00', '00FFFF', 'FFA500', '80b1d3', '00FFFF']
-const labels = ["input 1", "input2", "input3", "input4", "input5", "input6"]
 
 function BarGraphWidget() {
   const [sampleData, setSampleData] = useSyncedState('sampleData', [10, 20, 30, 40, 50, 60]);
@@ -13,16 +12,32 @@ function BarGraphWidget() {
   const barSpacing = 60
   const barBaseY = frame_h - 30;
   const labelY = barBaseY + 10;
-  const title = "Widget Bar Graph"
+  const title = "Bar Graph Widget"
   const incrementValue = (index: number) => {
     const newData = [...sampleData];
     newData[index] += 10;
     setSampleData(newData);
   };
-
+  const addBar = () => {
+    const newData = [...sampleData, 100];
+    setSampleData(newData);
+    console.log(sampleData);
+    console.log(newData);
+  }
   return (
-    <AutoLayout>
+    <AutoLayout
+      direction="vertical"
+      verticalAlignItems="end"
+      horizontalAlignItems={"center"}
+
+    >
     <Frame width={frame_w} height={frame_h}>
+      </Frame>
+      <AutoLayout
+        verticalAlignItems="end"
+        direction="horizontal"
+        spacing={30}
+        >
       <Text
         x={frame_w / 2 - 110}
         y={10}
@@ -31,13 +46,26 @@ function BarGraphWidget() {
         fill={`#00FF00`}
         >
         {title}
-        </Text>
-      {sampleData.map((value, index) => (
+      </Text>
+        {sampleData.map((value, index) => (
         <>
-        <AutoLayout></AutoLayout>
+        <AutoLayout
+        horizontalAlignItems="end"
+        direction="vertical"
+        spacing={10}
+        >
+
         <Text fontSize={32} width={42} x={barSpacing * index + 20} horizontalAlignText={'center'}>
         {value}
       </Text>
+      <Rectangle
+      key={`bar-${index}`}
+      x={barSpacing * index + 20}
+      y={barBaseY - value - 20}
+      width={barWidth}
+      height={value}
+      fill={`#${colors[index]}`}
+      />
       <SVG
       width={20}
       height={20}
@@ -51,26 +79,26 @@ function BarGraphWidget() {
       x={barSpacing * index + 26}
       y={barBaseY + 2}
     ></SVG>
-      <Rectangle
-      key={`bar-${index}`}
-      x={barSpacing * index + 20}
-      y={barBaseY - value - 20}
-      width={barWidth}
-      height={value}
-      fill={`#${colors[index]}`}
-      />
-      <AutoLayout></AutoLayout>
       <Text
         key={`label-${index}`}
         x={barSpacing * index + 25}
         y={labelY - 20}
         fontSize={12}
         >
-          {labels[index]}
+          input {index + 1}
           </Text>
+          </AutoLayout>
           </>
-       ))}
-    </Frame>
+          ))}
+ <SVG
+      src={`<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="30" height="30" rx="15" fill="white"/>
+      <path d="M15.9375 7.5H14.0625V14.0625H7.5V15.9375H14.0625V22.5H15.9375V15.9375H22.5V14.0625H15.9375V7.5Z" fill="black" fill-opacity="0.8"/>
+      <rect x="0.5" y="0.5" width="29" height="29" rx="14.5" stroke="black" stroke-opacity="0.1"/>
+      </svg>`}
+      onClick={addBar}
+    ></SVG>
+       </AutoLayout>
     </AutoLayout>
   )
 }
