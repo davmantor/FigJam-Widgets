13768333943a99c4061c7d2d13a52469d1584527
bonniewang0917 <swang526@ucsc.edu>
2024-07-11 23:12:20 -0700

    broken after trying to add interaction to add a bar

--- onboarding/SIP 2024/Bonniefolder/widget/widget-src/code.tsx
+++ onboarding/SIP 2024/Bonniefolder/widget/widget-src/code.tsx
@@ -5,6 +5,8 @@ const colors = ["8dd3c7", "ffffb3", "bebada", "fb8072", "80b1d3", "fdb462"];
 const labels = ["input 1", "input 2", "input 3", "input 4", "input 5", "input 6"];
 const sampleData = [10,20,30,40,50,60]
 
+
+
 function BarGraphWidget() {
   const [data, setData] = useSyncedState("data", sampleData)
   const frameWidth = 400;
@@ -26,9 +28,18 @@ const decrementValue = (index:number) => {
   newData[index] -= 10;
   setData(newData)
 }
+
+
+const addingbar = () =>
+const newdata = [...data,90]
+setData(newdata)
+console.log(data)
+console.log(newdata)
+
   
 return (
-  <AutoLayout direction="horizontal" x={10} y={10}>
+  <AutoLayout direction="horizontal" x={10} y={10} verticalAlignItems="end" horizontalAlignItems={"center"}>
+  <AutoLayout ></AutoLayout> 
     <Frame width={frameWidth} height={frameHeight}>
       <Text x={frameWidth/2 - 30} y={20} fontSize={16} fontWeight="bold">
         {title}
@@ -51,13 +62,24 @@ return (
           x={barSpacing * index + 32} y={barBaseY - value - 15} fontSize={12}>
             {value}
           </Text>
+          
+          <Input
+              type="text"
+              value={label}
+              onChange={() => handleLabelChange(index)}
+              onBlur={() => handleLabelBlur(index)}
+            />
+
+          
+          
+          
           <SVG
             key={`button-${index}`}
             width={20}
             height={20}
             x={barSpacing * index + 30}
             y={labelY + 20}
-            onClick={() => incrementValue(index)}
+           onClick={() => incrementValue(index)} 
             src={`<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
               <rect width="30" height="30" rx="15" fill="white"/>
               <path d="M15.9375 7.5H14.0625V14.0625H7.5V15.9375H14.0625V22.5H15.9375V15.9375H22.5V14.0625H15.9375V7.5Z" fill="black" fill-opacity="0.8"/>
@@ -72,6 +94,20 @@ return (
             y={labelY + 45}
             onClick={() => decrementValue(index)}
             src={`<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="30" height="30" rx="15" fill="white"/>
+        <rect x="7.5" y="14.0625" width="15" height="1.875" fill="black" fill-opacity="0.8"/>
+        <rect x="0.5" y="0.5" width="29" height="29" rx="14.5" stroke="black" stroke-opacity="0.1"/>
+        </svg>`}
+          />
+
+<SVG   
+  key={`button-${index}`}
+   width={20}
+   height={20}
+   x={30}
+   y={labelY + 20}
+   onClick={() => addingbar(value)} 
+   src={`<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
               <rect width="30" height="30" rx="15" fill="white"/>
               <path d="M15.9375 7.5H14.0625V14.0625H7.5V15.9375H14.0625V22.5H15.9375V15.9375H22.5V14.0625H15.9375V7.5Z" fill="black" fill-opacity="0.8"/>
               <rect x="0.5" y="0.5" width="29" height="29" rx="14.5" stroke="black" stroke-opacity="0.1"/>
@@ -83,4 +119,5 @@ return (
   </AutoLayout>
 );
 }
+
 widget.register(BarGraphWidget)
