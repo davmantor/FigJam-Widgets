b91a9144d228df001ee882fa59b76942c9367f42
bonniewang0917 <swang526@ucsc.edu>
2024-07-21 18:51:29 -0700

    finishing minusing bar part

--- onboarding/SIP 2024/Bonniefolder/widget/widget-src/code.tsx
+++ onboarding/SIP 2024/Bonniefolder/widget/widget-src/code.tsx
@@ -35,7 +35,15 @@ setData(newdata)
 console.log(data)
 console.log(newdata)
 }
-  
+
+
+const minusingbar = () => {
+  console.log("moving bar ",data)  
+  const newdata = data.slice(0, -1);
+  setData(newdata)
+  console.log(newdata)
+  }
+
 return (
 <AutoLayout
   direction="vertical"
@@ -123,7 +131,21 @@ spacing={20} >
               <path d="M15.9375 7.5H14.0625V14.0625H7.5V15.9375H14.0625V22.5H15.9375V15.9375H22.5V14.0625H15.9375V7.5Z" fill="black" fill-opacity="0.8"/>
               <rect x="0.5" y="0.5" width="29" height="29" rx="14.5" stroke="black" stroke-opacity="0.1"/>
               </svg>`}
-          />
+              />
+
+        <SVG   
+       
+        width={20}
+        height={20}
+
+        onClick={() => minusingbar()} 
+        src={`<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect width="30" height="30" rx="15" fill="white"/>
+              <rect x="7.5" y="14.0625" width="15" height="1.875" fill="black" fill-opacity="0.8"/>
+              <rect x="0.5" y="0.5" width="29" height="29" rx="14.5" stroke="black" stroke-opacity="0.1"/>
+              </svg>`}
+              />
+          
 
 
 
