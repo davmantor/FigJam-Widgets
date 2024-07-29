b91cad310430732f54ab4293e01b057d40080e89
pyang26 <dhyang10004@gmail.com>
2024-07-29 10:20:56 -0700

    ensure that + button disappears upon form submission

--- polling-widget/widget-src/code.tsx
+++ polling-widget/widget-src/code.tsx
@@ -112,6 +112,7 @@ function PollingWidget() {
           isEditable={!isSubmitted}
         />
       ))}
+      {!isSubmitted && (
       <SVG
         src={`<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
               <rect width="30" height="30" rx="15" fill="white"/>
@@ -120,6 +121,7 @@ function PollingWidget() {
             </svg>`}
         onClick={handleAddTextField}
       />
+        )}
       {!isSubmitted && (
         <AutoLayout
           width={100}
