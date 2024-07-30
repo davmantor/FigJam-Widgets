cd3877cc4d4a16c5aabb8b17335721d8f9dfab76
pyang26 <dhyang10004@gmail.com>
2024-07-30 11:21:29 -0700

    First text field style change, improve readability of code

--- polling-widget/widget-src/code.tsx
+++ polling-widget/widget-src/code.tsx
@@ -2,7 +2,6 @@
 // Once submitted, the text option becomes non-editable. The widget consists of two main components:
 // 1. EditableText: Handles displaying and editing of a single text entry.
 // 2. PollingWidget: Manages the overall layout and state, including submission of the text entry.
-
 const { widget } = figma;
 const { useSyncedState, AutoLayout, Input, Text, SVG, Frame } = widget;
 
@@ -12,19 +11,21 @@ interface EditableTextProps {
   initialValue: string; // The initial text to show in the component.
   onValueChange: (index: number, newValue: string) => void; // What to do when the text changes.
   isEditable: boolean; // Can we edit this text?
-  fill: string;
+  fill?: string;
   placeholder?: string;
 }
 
 // This component can either let you edit text or just show you the text.
-function EditableText({ index, initialValue, onValueChange, isEditable, fill, placeholder}: EditableTextProps) {
+function EditableText({ index, initialValue, onValueChange, isEditable, fill, placeholder }: EditableTextProps) {
   // 'isEditing' tracks if we're currently editing text. 'setIsEditing' changes this status.
   const [isEditing, setIsEditing] = useSyncedState(`isEditing-${index}`, false);
   // 'inputValue' holds the current text. 'setInputValue' updates this text.
   const [inputValue, setInputValue] = useSyncedState(`inputValue-${index}`, initialValue);
 
+  const isFirstTextEntry = (index === -1);
+
   // This is what the component looks like on the screen.
-  // creates a vertically stacked container that either displays an editable text input 
+  // Creates a vertically stacked container that either displays an editable text input 
   // (when editing is allowed and active) or static text with an optional edit icon (when editing is not active). 
   // The appearance and behavior dynamically change based on whether the text is currently being edited or not.
   return (
@@ -49,7 +50,13 @@ function EditableText({ index, initialValue, onValueChange, isEditable, fill, pl
         />
       ) : (
         <AutoLayout spacing={8} verticalAlignItems={'center'}>
-          <Text fontSize={16}>{inputValue || placeholder || "Enter option"}</Text>
+          <Text
+            fontSize={16}
+            fill={inputValue ? '#000000' : (isFirstTextEntry ? '#808080' : '#000000')}  
+            fontWeight={inputValue ? 'normal' : (isFirstTextEntry ? 'bold' : 'normal')} 
+          >
+            {inputValue || placeholder || "Enter option"}
+          </Text>
           {isEditable && (
             <SVG
               src={`<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -105,26 +112,23 @@ function PollingWidget() {
       fill={'#FFFFFF'}  // Background color.
       stroke={'#E6E6E6'}  // Border color.
     >
-            <AutoLayout
-      fill={'#808080'}
-      stroke={'#E7E7E7'}
-      direction="vertical"
-
+      <AutoLayout
+        stroke={'#E7E7E7'}
+        direction="vertical"
         spacing={12}
         padding={12}
         cornerRadius={8}
-
       >
-      <EditableText
-              key={-1}
-              index={-1}
+        <EditableText
+          key={-1}
+          index={-1}
           initialValue={''}
           onValueChange={handleValueChange}
           isEditable={!isSubmitted}
           placeholder="Enter poll question here"
           fill={'FF0000'}
         />
-        </AutoLayout>
+      </AutoLayout>
       {textArray.map((item, index) => (
         <EditableText
           key={index}
@@ -137,15 +141,15 @@ function PollingWidget() {
       ))}
 
       {!isSubmitted && (
-      <SVG
-        src={`<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect width="30" height="30" rx="15" fill="white"/>
-              <path d="M15.9375 7.5H14.0625V14.0625H7.5V15.9375H14.0625V22.5H15.9375V15.9375H22.5V14.0625H15.9375V7.5Z" fill="black" fill-opacity="0.8"/>
-              <rect x="0.5" y="0.5" width="29" height="29" rx="14.5" stroke="black" stroke-opacity="0.1"/>
-            </svg>`}
-        onClick={handleAddTextField}
-      />
-        )}
+        <SVG
+          src={`<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="30" height="30" rx="15" fill="white"/>
+                <path d="M15.9375 7.5H14.0625V14.0625H7.5V15.9375H14.0625V22.5H15.9375V15.9375H22.5V14.0625H15.9375V7.5Z" fill="black" fill-opacity="0.8"/>
+                <rect x="0.5" y="0.5" width="29" height="29" rx="14.5" stroke="black" stroke-opacity="0.1"/>
+              </svg>`}
+          onClick={handleAddTextField}
+        />
+      )}
       {!isSubmitted && (
         <AutoLayout
           width={100}
