6305437d9677895f1b3a696c07bb9bfff8993bde
cabalex <31020729+cabalex@users.noreply.github.com>
2024-11-14 15:17:17 -0800

    Likert: Test different starting state

--- likert/widget-src/code.tsx
+++ likert/widget-src/code.tsx
@@ -7,13 +7,16 @@ interface Response {
   comment: string;
 }
 
+const defaultColor = "#FFD966";
+const colors = ["#FFAFA3", "#FFC470", "#FFD966", "#85E0A3", "#80CAFF", "#FFADE7", "#D9B8FF", "#E6E6E6", "AFBCCF"];
+
 function Widget() {
+  const [isFreeResponse, setIsFreeResponse] = useSyncedState("isFreeResponse", true);
   const [question, setQuestion] = useSyncedState("question", "");
   const [questionAuthor, setQuestionAuthor] = useSyncedState<null|Response['user']>("questionAuthor", null);
+  const [labels, setLabels] = useSyncedState<string[]>("labels", ["Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"]);
   const [responses, setResponses] = useSyncedState<Response[]>("responses", []);
   const widgetId = useWidgetNodeId();
-  const scaleLabels = ["Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"];
-  const colors = ["#FFAFA3", "#FFC470", "#FFD966", "#85E0A3", "#80CAFF", "#FFADE7", "#D9B8FF", "#E6E6E6", "AFBCCF"];
 
   useEffect(() => {
     figma.ui.onmessage = (msg) => {
@@ -34,7 +37,7 @@ function Widget() {
   const openPluginWindow = (ratingIndex: number) => {
     return new Promise<void>((resolve) => {
       figma.showUI(__html__, { width: 300, height: 300, title: question });
-      figma.ui.postMessage({ type: "init", user: currentUser, rating: ratingIndex, question });
+      figma.ui.postMessage({ type: "init", user: currentUser, rating: ratingIndex, question, isFreeResponse });
     });
   };
 
@@ -54,9 +57,14 @@ function Widget() {
 
   const createCopy = async (res: Response) => {
     const node = await figma.getNodeByIdAsync(widgetId) as WidgetNode;
+
+    // some LLM magic here...
+
     const clone = node.cloneWidget({
-      question: res.comment,
+      question: "LLM question generated from \"" + res.comment + "\"",
       questionAuthor: res.user,
+      isFreeResponse: false,
+      labels: ["Custom", "Scale", "From", "LLM", "Response", "And", "More"],
       responses: [],
     });
     clone.y = node.y + node.height + 16;
@@ -67,7 +75,7 @@ function Widget() {
   const optionsWithResponses = new Set(responses.map(res => res.rating));
 
   return (
-    <AutoLayout direction="vertical" padding={16} spacing={4} fill={'#ffffff'} cornerRadius={10}>
+    <AutoLayout width={1032} direction="vertical" padding={16} spacing={4} fill={'#ffffff'} cornerRadius={10}>
       <AutoLayout width={'fill-parent'} cornerRadius={{topLeft: 5, topRight: 5}} verticalAlignItems="center" spacing={4} padding={8} fill={'#eeeeee'}>
         <Input placeholder="Set a title..." fontSize={18} width={'fill-parent'} value={question} onTextEditEnd={(e) => setQuestion(e.characters)} />
         {questionAuthor && <>
@@ -75,39 +83,49 @@ function Widget() {
           <Image src={questionAuthor.icon} width={20} height={20} cornerRadius={10} />
         </>}
       </AutoLayout>
-      <AutoLayout width={1000} spacing={4}>
-        {scaleLabels.map((label, index) => (
-          <AutoLayout onClick={openPluginWindow.bind(null, index + 1)} width={'fill-parent'} height={'fill-parent'} minHeight={150} key={index} direction="vertical" spacing={4} padding={8} fill={colors[index % colors.length]} strokeWidth={2} hoverStyle={{stroke: "#111111", opacity: 0.8}}>
-            <Text>{label}</Text>
-            <AutoLayout spacing={4} wrap={true} width={'fill-parent'}>
-              {responses
-                .filter((res) => res.rating === index + 1)
-                .map((res, i) => (
-                  <Image key={i} src={res.user.icon} width={20} height={20} cornerRadius={10} />
-                ))}
+      {!isFreeResponse && (<>
+        <AutoLayout width={1000} spacing={4}>
+          {labels.map((label, index) => (
+            <AutoLayout onClick={openPluginWindow.bind(null, index + 1)} width={'fill-parent'} height={'fill-parent'} minHeight={150} key={index} direction="vertical" spacing={4} padding={8} fill={colors[index % colors.length]} strokeWidth={2} hoverStyle={{stroke: "#111111", opacity: 0.8}}>
+              <Text>{label}</Text>
+              <AutoLayout spacing={4} wrap={true} width={'fill-parent'}>
+                {responses
+                  .filter((res) => res.rating === index + 1)
+                  .map((res, i) => (
+                    <Image key={i} src={res.user.icon} width={20} height={20} cornerRadius={10} />
+                  ))}
+              </AutoLayout>
             </AutoLayout>
-          </AutoLayout>
-        ))}
-      </AutoLayout>
-      {responses.length > 0 && <>
-        <Rectangle width={'fill-parent'} height={6} fill={'#ffffff'} />
-        <AutoLayout spacing={4} cornerRadius={10}>
-          {scaleLabels.map((label, index) =>
-            {return optionsWithResponses.has(index + 1) && <AutoLayout key={index} verticalAlignItems="center" horizontalAlignItems="center" padding={{left: 4}} width={(1000 - (optionsWithResponses.size - 1) * 4) / responses.length * responses.filter(x => x.rating - 1 === index).length} height={16} fill={colors[index % colors.length]}>
-              <Text fontSize={10} fill={'#333333'}>{Math.round(responses.filter(x => x.rating - 1 === index).length / responses.length * 100)}%</Text>
-            </AutoLayout>}
-          )}
+          ))}
         </AutoLayout>
+        {responses.length > 0 && (<>
+          <Rectangle width={'fill-parent'} height={6} fill={'#ffffff'} />
+          <AutoLayout spacing={4} cornerRadius={10}>
+            {labels.map((label, index) =>
+              {return optionsWithResponses.has(index + 1) && <AutoLayout key={index} verticalAlignItems="center" horizontalAlignItems="center" padding={{left: 4}} width={(1000 - (optionsWithResponses.size - 1) * 4) / responses.length * responses.filter(x => x.rating - 1 === index).length} height={16} fill={colors[index % colors.length]}>
+                <Text fontSize={10} fill={'#333333'}>{Math.round(responses.filter(x => x.rating - 1 === index).length / responses.length * 100)}%</Text>
+              </AutoLayout>}
+            )}
+          </AutoLayout>
+        </>)}
+        </>
+      )}
+      {isFreeResponse && (
+        <AutoLayout onClick={openPluginWindow.bind(null, -1)} width={'fill-parent'} height={50} direction="vertical" spacing={4} padding={8} fill={defaultColor} strokeWidth={2} cornerRadius={{bottomLeft: 5, bottomRight: 5}} hoverStyle={{stroke: "#111111", opacity: 0.8}}>
+          <Text>Click here to enter a response</Text>
+        </AutoLayout>
+      )}
+      {responses.length > 0 && <>
         <Rectangle width={'fill-parent'} height={6} fill={'#ffffff'} />
         <Text>{responses.length} {responses.length === 1 ? "response" : "responses"}</Text>
         {responsesWithoutComments.length !== responses.length &&
           <AutoLayout direction="horizontal" wrap={true} spacing={4} width={1000}>
             {
               responses.filter(res => res.comment.length > 0).map((res, i) => (
-                <AutoLayout onClick={createCopy.bind(null, res)} width={(1000 - 16) / 5} minHeight={100} fill={colors[(res.rating - 1) % colors.length]} strokeAlign={"inside"} key={i} direction="vertical" spacing={4} padding={8}>
+                <AutoLayout onClick={createCopy.bind(null, res)} width={(1000 - 16) / 5} minHeight={100} fill={isFreeResponse ? colors[i % colors.length] : colors[(res.rating - 1) % colors.length]} strokeAlign={"inside"} key={i} direction="vertical" spacing={4} padding={8}>
                   <Text width={'fill-parent'} height={'fill-parent'}>{res.comment}</Text>
                   <AutoLayout spacing={4} verticalAlignItems="end" width={'fill-parent'}>
-                    <Text fill={'#000000'} opacity={0.5} fontSize={10}>{scaleLabels[res.rating - 1]}</Text>
+                    <Text fill={'#000000'} opacity={0.5} fontSize={10}>{isFreeResponse ? res.user.name : labels[res.rating - 1]}</Text>
                     <Rectangle height={'fill-parent'} width={'fill-parent'} />
                     <Image src={res.user.icon} width={16} height={16} cornerRadius={10} />
                   </AutoLayout>
